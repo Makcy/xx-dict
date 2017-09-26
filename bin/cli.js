b67ec3164dc4725b4e9568f9435a1cb5a105be12
Makcy <644215038@qq.com>
2017-09-26 23:58:29 +0800

    [ADD]add bin cli tool

--- bin/cli.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node 
-
-console.log('Hello I am bin cli');
--- src/lookup.js
+++ src/lookup.js
@@ -1,13 +1,5 @@
 const ora = require('ora');
-const cliSpinners = require('cli-spinners');
-const spinner = ora(cliSpinners.dots.frames);
 
-function lookup() {
-  // setTimeout(() => {
-  //   spinner.color = 'red';
-  //   spinner.text = 'Hello World';
-  // }, 10)
-
-  console.log(spinner)
-}
-module.exports = lookup;
+module.exports = () => {
+  console.log('Hello LookUp');
+};
