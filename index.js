89c8de84013bd32161cae1367da29ca8bf31d5cb
Makcy <644215038@qq.com>
2017-09-28 17:10:18 +0800

    add

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 'use strict';
 
-exports = require('./src/lookup');
+module.exports = require('./src/lookup');
--- src/lookup.js
+++ src/lookup.js
@@ -1,6 +1,24 @@
-const ora = require('ora');
-const cliSpinner = require('cli-spinners');
+const request = require('request');
+const cheerio = require('cheerio');
+const urlencode = require('urlencode');
+const isChinese = require('is-chinese')
 
-module.exports = () => {
-  console.log('Look Up...');
+function lookup(word) {
+  //  TODO: Ugly Code！！！
+  const isCn = isChinese(word);
+  const URL = isCn ? `http://dict.youdao.com/w/eng/${urlencode(word)}`:`http://dict.youdao.com/w/${word}`
+  request(URL, (err, response, body) => {
+    const $ = cheerio.load(body);
+    if(isCn){
+      $('div.trans-container > ul').find('p.wordGroup').each(function(i,elm){
+        var line = $(this).text().replace(/\s+/g," ");
+        console.log(line);
+      });
+    }else{
+      console.log($('div#phrsListTab > div.trans-container > ul').text());
+    }
+  });
 };
+
+
+module.exports = lookup;
