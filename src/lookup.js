d0747cf9179059bbcb833c653a7205166295ff4a
Makcy <644215038@qq.com>
2017-10-12 23:12:25 +0800

    remove package

--- src/lookup.js
+++ src/lookup.js
@@ -1,12 +1,11 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const urlencode = require('urlencode');
-const isChinese = require('is-chinese')
 
 function lookup(word) {
   //  TODO: Ugly Code！！！
   if (!word) return;
-  const isCn = isChinese(word);
+  let isCn = /^[\u4E00-\u9FA5]+$/.test(word);
   const URL = isCn ? `http://dict.youdao.com/w/eng/${urlencode(word)}`:`http://dict.youdao.com/w/${word}`
   request(URL, (err, response, body) => {
     const $ = cheerio.load(body);
